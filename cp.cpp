8f3d0b4c4b18e82b74545a211b5aa1fb22493520
kishimoto <kishimoto@pc002.ei.sanken.osaka-u.ac.jp>
2018-10-16 16:44:51 +0900

    second commit

--- cp.cpp
+++ cp.cpp
@@ -5,42 +5,48 @@
 #include <cstdlib> // EXIT_FAILURE のため
 
 //using namespace std;
-//void ReadFile(std::string read_triple_file, 
+//std::vector< std::vector <int> > a;
+std::vector< std::vector <std::string> > a;
+std::vector<std::string> b(3);
 
-int main(int argc, char *argv[]) {
-    //std::vector< std::vector <std::string> > a;
-    std::vector< std::vector <int> > a;
-    //std::vector<std::string> b(3);
-    std::vector<int> b(3);
-    //std::vector<std::vector <std::string> > a(10, std::vector<std::string>(13));
-
+int ReadFile(std::string read_triple_file){
     std::fstream fs;
-    fs.open(argv[1], std::ios::in);
+    fs.open(read_triple_file, std::ios::in);
+    //fs.open("test.dat", std::ios::in);
     if(! fs.is_open()) {
-        // exit だけでなく return の戻り値としても使用できる。
         return EXIT_FAILURE;
     }
-    do {
-
+    while (! fs.eof()){
+    //do {
     	// cin のように一行読み込む
-	//std::string l,m,n;
-	int l,m,n;
+	std::string l,m,n;
+	//int l,m,n;
     	fs >> l >> m >> n;
 	b[0] = l;
 	b[1] = m;
 	b[2] = n;
 
 	std::cout << b[0] << b[1] << b[2] << std::endl;
-	a.push_back(b);
-
-    }while (! fs.eof()); // eof: ファイル終端にファイルポインタがあってもfalseです。
-    // 終端を越えようとしていればtrueです。そのため、input.bin が空であっても最初はfalseです
-    // よって、do-while であっても while であっても表示結果は同じで最低1ループは実行されます。
-    // ここでは現実をそのまま直感的に表現したdo-whileを採用しました。
-    
-    std::cout << a[0][0] + a[0][1] << std::endl;
+	//a.push_back(b);
+    }
+    //}while (! fs.eof()); 
 
     fs.close(); // デストラクタでも閉じてくれますが、明示的に閉じる習慣を。
+    return 0;
+
+}
+
+int main(int argc, char *argv[]) {
+//int main(){
+    //std::vector< std::vector <std::string> > a;
+    //std::vector<std::string> b(3);
+    //std::vector<std::vector <std::string> > a(10, std::vector<std::string>(13));
+    
+    std::string read_triple_file = argv[1];
+
+    
+    //std::cout << a[0][0] + a[0][1] << std::endl;
+
 
     /*
     for(int i=0;i<2;i++){
@@ -50,5 +56,6 @@ int main(int argc, char *argv[]) {
 	std::cout<<std::endl;
     }
     */
+    ReadFile(read_triple_file);
     return 0;
 }
