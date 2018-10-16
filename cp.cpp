22659c930f803a8f4ef42e6b530519e9a1c096ea
kishimoto <kishimoto@pc002.ei.sanken.osaka-u.ac.jp>
2018-10-16 16:49:10 +0900

    third commit

--- cp.cpp
+++ cp.cpp
@@ -6,30 +6,29 @@
 
 //using namespace std;
 //std::vector< std::vector <int> > a;
-std::vector< std::vector <std::string> > a;
-std::vector<std::string> b(3);
+std::vector< std::vector <int> > a;
+std::vector<int> b(3);
 
 int ReadFile(std::string read_triple_file){
     std::fstream fs;
+    int l,m,n;
     fs.open(read_triple_file, std::ios::in);
     //fs.open("test.dat", std::ios::in);
     if(! fs.is_open()) {
         return EXIT_FAILURE;
     }
-    while (! fs.eof()){
+    while (fs >> l >> m >> n){
     //do {
     	// cin のように一行読み込む
-	std::string l,m,n;
 	//int l,m,n;
-    	fs >> l >> m >> n;
+    	//fs >> l >> m >> n;
 	b[0] = l;
 	b[1] = m;
 	b[2] = n;
 
-	std::cout << b[0] << b[1] << b[2] << std::endl;
-	//a.push_back(b);
+	//std::cout << b[0] << b[1] << b[2] << std::endl;
+	a.push_back(b);
     }
-    //}while (! fs.eof()); 
 
     fs.close(); // デストラクタでも閉じてくれますが、明示的に閉じる習慣を。
     return 0;
