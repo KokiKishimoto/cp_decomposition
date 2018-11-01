f2654f7b618895541f085550ccaed53a81ddae09
kishimoto <kishimoto@airst402.ei.sanken.osaka-u.ac.jp>
2018-11-01 17:56:23 +0900

    bug fix

--- compiletest.sh
+++ compiletest.sh
@@ -1,5 +1,5 @@
 clang++ -std=c++11 -O2 testdata.cpp
 ./a.out -testfile testid\
-	-subject_model ./model/60_subject.txt\
-	-object_model ./model/60_object.txt\
-	-relation_model ./model/60_relation.txt
+	-subject_model ./model/180_subject.txt\
+	-object_model ./model/180_object.txt\
+	-relation_model ./model/180_relation.txt
