7fe65b84c807bc801996129b802d5d5217d15c53
kishimoto <kishimoto@kishimotos-MacBook-Pro.local>
2018-11-02 17:02:41 +0900

    remoce residual

--- compiletest.sh
+++
@@ -1,5 +0,0 @@
-clang++ -std=c++11 -O2 testdata.cpp
-./a.out -testfile testid\
-	-subject_model ./model/100_subject.txt\
-	-object_model ./model/100_object.txt\
-	-relation_model ./model/100_relation.txt
