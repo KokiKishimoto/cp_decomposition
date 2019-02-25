cbe95e2d3d4b55632f0ec8a6a4645195542b5305
KokiKishimoto <kishimoto@ei.sanken.osaka-u.ac.jp>
2019-02-25 20:26:56 +0900

    fi

--- train_and_test.sh
+++ train_and_test.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-unzip ./FB15k-237.zip
-
 model_dir=./model
 
 make all
 
-./add_rev.out -file_name ./FB15k-237/train.txt -rev_filename ./FB15k-237/a.txt
+./add_rev.out -file_name ./datasets/FB15k-237/train.txt -rev_filename ./datasets/FB15k-237/train_rev.txt
 
 mkdir -p $model_dir
-./quantizedcp.out -train ./FB15k-237/a.txt \
+./quantizedcp.out -train ./datasets/FB15k-237/train_rev.txt \
 	-dimension 200 \
-	-iteration 100 \
+	-iteration 400 \
 	-rate 0.025 \
 	-lambda 0.0001 \
 	-model_dir $model_dir
 
-./testcp.out -train ./FB15k-237/a.txt \
-	-test ./FB15k-237/test.txt \
-	-valid ./FB15k-237/valid.txt \
-	-subject_model "${model_dir}/100_subject.txt" \
-	-object_model "${model_dir}/100_object.txt" \
-	-relation_model "${model_dir}/100_relation.txt"
+./testcp.out -train ./datasets/FB15k-237/train_rev.txt \
+	-test ./datasets/FB15k-237/test.txt \
+	-valid ./datasets/FB15k-237/valid.txt \
+	-subject_model "${model_dir}/400_subject.txt" \
+	-object_model "${model_dir}/400_object.txt" \
+	-relation_model "${model_dir}/400_relation.txt"
